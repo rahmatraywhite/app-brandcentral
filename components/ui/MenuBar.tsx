669d6bb9be98756d7552438a24dc9b0b36980e06
rahmatraywhite <rahmat.hidayat@raywhite.co.id>
2024-04-30 16:53:49 +0700

    add menu bar

--- components/ui/MenuBar.tsx
+++ components/ui/MenuBar.tsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import Documents from 'react-native-vector-icons/MaterialCommunityIcons';
 import Profile from 'react-native-vector-icons/FontAwesome6';
 import Home from 'react-native-vector-icons/Ionicons';
 
 interface MenuItem {
   name: string;
+  routeName: string;
   icon: JSX.Element;
 }
+
 export default function MenuBar() {
+  const navigation = useNavigation();
+  const route = useRoute();
   const [activeTab, setActiveTab] = useState<string | null>(null);
+
+  useEffect(() => {
+    setActiveTab(route.name);
+  }, [route]);
+
   const menu: MenuItem[] = [
     {
       name: 'Home',
+      routeName: 'Home',
       icon: (
         <Home
           name="home"
@@ -23,16 +34,18 @@ export default function MenuBar() {
     },
     {
       name: 'Save Documents',
+      routeName: 'Saved',
       icon: (
         <Documents
           name="download-circle"
           size={30}
-          color={activeTab === 'Save Documents' ? 'yellow' : 'white'}
+          color={activeTab === 'Saved' ? 'yellow' : 'white'}
         />
       ),
     },
     {
       name: 'Profile',
+      routeName: 'Profile',
       icon: (
         <Profile
           name="circle-user"
@@ -43,17 +56,20 @@ export default function MenuBar() {
     },
   ];
 
+  const handleNavigation = (routeName: string) => {
+    navigation.navigate(routeName as never);
+  };
+
   return (
     <View className="py-[20px] absolute  bottom-0 left-0 right-0 px-[10px] bg-[#3F4447]">
       <View className="flex flex-row justify-center gap-[56px]">
         {menu.map((item) => (
           <TouchableOpacity
             key={item.name}
-            onPress={() => setActiveTab(item.name)}
-            onPressOut={() => setActiveTab(null)}>
+            onPress={() => handleNavigation(item.routeName)}>
             <View style={{ alignItems: 'center' }}>
               {item.icon}
-              <Text className="text-white mt-[5px]">{item.name}</Text>
+              <Text style={{ color: 'white', marginTop: 5 }}>{item.name}</Text>
             </View>
           </TouchableOpacity>
         ))}
